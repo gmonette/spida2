5392a87f01825c2456608a9190e4d13d376a2e8d
Georges Monette <georges@yorku.ca>
2017-05-05 15:01:18 -0400

    add 'name' to supplant 'autonym'

--- R/auto.R
+++ R/auto.R
@@ -14,4 +14,24 @@ auto <- function(x, force = FALSE) {
 #' @rdname auto
 #' @export
 autonym <- auto
+#'
+#' Add or modify a name of an object
+#'
+#' @param x object
+#' @param nam name
+#' @return x with a modified name
+#' @examples
+#' x <- as.list(letters[1:3])
+#' x
+#' library(magrittr)
+#' x  %>% name
+#' gsub_ <- function(x,pat,to,...) gsub(pat,to,x,...)
+#' x %>% name()
+#' x  %>% name %>% name(toupper)
+#' x  %>% name %>% name(gsub_, '(.*)', 'time_\\1')
 #' @export
+name <- function(x, nam = x,...) {
+  if(is.function(nam)) nam <- nam(names(x),...)
+  names(x) <- nam
+  x
+}
