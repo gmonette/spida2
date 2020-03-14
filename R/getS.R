a4da533b4fe4a687335f2dd2ccaf31bfdee11653
Georges Monette <georges@yorku.ca>
2020-03-14 00:34:24 -0400

    add getS for samples from a stanfit object

---
+++ R/getS.R
@@ -0,0 +1,17 @@
+#' Extract sample and sampling parameters from Bayesian posterior
+#' 
+#' @param x a fitted Bayesian regression object, for example,
+#'        using \code{rstan}.  
+#' @export
+getS <- function(x,...) UseMethod('getS')
+getS.default <- function(x,...) {
+  stop('No method implemented for object of class ',class(x),'\n')
+}
+getS.stanfit <- function(sf) {
+  # function to create a data frame
+  # of samples from a stanfit object
+  # together with sampler parameters
+  sam <- rstan:::as.data.frame.stanfit(sf)
+  pars <- as.data.frame(do.call(rbind,rstan::get_sampler_params(sf, inc_warmup = FALSE)))
+  cbind(sam, pars)
+}
