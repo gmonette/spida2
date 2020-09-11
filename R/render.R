d583a3d80299b9df5099af56b3579bd7a14b9398
Georges Monette <georges@yorku.ca>
2020-09-11 16:43:59 -0400

    add render

---
+++ R/render.R
@@ -0,0 +1,9 @@
+#' Render with Rmarkdown and keep intermediate files
+#' 
+#' Following https://github.com/rstudio/rmarkdown/issues/17 
+#' add the YAML: \code{knit: spida2::render_keep} to a .R Rmarkdown 
+#' document with output YAML \code{output: html_document}.
+#' 
+render_keep <- function(input, encoding) {
+rmarkdown::render(input, clean = FALSE, encoding = encoding)
+}
