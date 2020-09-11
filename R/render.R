46abba9c9806982f164960437ca1bbba689feac7
Georges Monette <georges@yorku.ca>
2020-09-11 16:48:24 -0400

    add render_keep

--- R/render.R
+++ R/render.R
@@ -4,6 +4,7 @@
 #' add the YAML: \code{knit: spida2::render_keep} to a .R Rmarkdown 
 #' document with output YAML \code{output: html_document}.
 #' 
+#' @export
 render_keep <- function(input, encoding) {
 rmarkdown::render(input, clean = FALSE, encoding = encoding)
 }
