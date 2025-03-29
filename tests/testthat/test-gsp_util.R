6a1d5fb079eed5817a7b9d752caafc40b12c2e35
Georges Monette <georges@yorku.ca>
2025-03-29 00:43:41 -0400

    change Description to version 0.2.3

--- tests/testthat/test-gsp_util.R
+++ tests/testthat/test-gsp_util.R
@@ -4,7 +4,7 @@ test_that("Polyshift works", {
       coef <- 1:4
       x <- 4
       coef*(x^(0:3))
-      sum((PolyShift(3,4) %*% coef)*(x-3)^(0:3) ) -
+      sum((Po(3,4) %*% coef)*(x-3)^(0:3) ) -
       sum(coef*(x^(0:3)))
     }, 0)
 })
