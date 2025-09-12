3c25c0a2f246cfe1ef5ebe12946ad5d4832c6820
Georges Monette <georges@yorku.ca>
2025-09-12 09:36:54 -0400

    removing pdInd to avoid conflicts with nlme fork

--- R2/pdInd.R
+++ R2/pdInd.R
@@ -463,25 +463,5 @@ fit6b$modelStruct$reStruct$school
   
   unclass(ff2)
   unclass(ff3)
-ga(pdMatrix.pdMat)
-
-library(nlme)
-library(spida2)
-fit <- lme(mathach ~ ses + Sex, hs, 
-           random = ~ 1 + ses + Sex|school,
-           control = list(msMaxIter=1000))  
-summary(fit)
-fit2 <- lme(mathach ~ ses + Sex, hs, 
-           random = list(school = pdSym(~ 1 + ses + Sex)),
-           control = list(msMaxIter=1000))  
-summary(fit2)
-fit2$modelStruct$reStruct
-debug(pdInd)
-fit6b <- lme(mathach ~ ses + Sex + Minority, hs,
-             random = 
-               list( 
-                 school = pdInd( ~ 1 + ses + Sex + Minority, cov = cov)),
-             control = list(msVerbose=T,returnObject=T,msMaxIter=1000))
+  
 }
-
-
