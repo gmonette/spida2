5deef198be753889820f1004a94daea6262de63d
Georges Monette <georges@yorku.ca>
2026-01-11 22:41:49 -0500

    remove grepv

--- R/grepv.R
+++ R/grepv.R
@@ -2,7 +2,9 @@
 #'
 #' Equivalent of grep(..., value = TRUE)
 #'
+#' Now obsolete since the addition of the same command to base R.
+#' 
 #' @param \dots arguments to \code{\link{grep}}
 #' @seealso \code{\link{grepl}}
 #' @export
-grepv <- function(...) grep( ..., value = TRUE)
+grepv2 <- function(...) grep( ..., value = TRUE)
